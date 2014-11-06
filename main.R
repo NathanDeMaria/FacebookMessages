72dcc35b69f0581e19080f7645d5a2349124d086
NathanDeMaria <naterstd@gmail.com>
2014-11-05 21:53:42 -0600

    Count what we ++'d, a little disappointing

--- main.R
+++ main.R
@@ -1,47 +1,23 @@
 
-library(httr)
-library(data.table)
-library(dplyr)
-library(lubridate)
+library(stringr)
+source('get.R')
 
-source('app_settings.R')
+comments <- get_comments()
 
-# do me/inbox
-base_url <- 'https://graph.facebook.com'
-query <- '/me/inbox'
+plussed <- lapply(comments$text, function(message_text) {
+	
+	# split into words
+	words <- unlist(str_split(message_text, '\\s+'))
+	
+	matches <- words[regexpr('\\+\\+', words) > 0]
+	
+	matches <- gsub('\\+\\+', '', matches)
+	matches <- gsub('[[:punct:]]', '', matches)
+	matches <- gsub('[[:cntrl:]]', '', matches)
+	matches <- gsub('\\d+', '', matches)
+	matches <- iconv(matches, 'ASCII', sub = "byte")
+	tolower(matches)
+})
 
-url <- paste0(base_url, query, '?access_token=', app_settings['token']$value)
-rsp <- content(GET(url))
-
-datas <- rsp$data
-the_convo <- datas[[which(sapply(datas, function(dat) {dat$id == app_settings['conversation_id']$value}))]]
-comments_list <- the_convo$comments
-
-parse_comment <- function(comment) {
-	message <- comment$message
-	if(is.null(message)){
-		message <- ''
-	}
-	data.frame(
-			id = comment$id,
-			name = comment$from$name,
-			text = message,
-			time = comment$created_time,
-			stringsAsFactors=F
-		)
-}
-
-comments_frames <- lapply(comments_list$data, parse_comment)
-
-while(!is.null(comments_list$paging$`next`)) {
-	comments_list <- content(GET(comments_list$paging$`next`))
-	comments_frames <- c(comments_frames, lapply(comments_list$data, parse_comment))
-}
-
-comments <- data.table(rbind_all(comments_frames))
-comments[,time:=ymd_hms(time)]
-setkey(comments, id)
-
-
-
-
+plus_table <- table(unlist(plussed))
+print(plus_table[order(plus_table, decreasing = T)])
