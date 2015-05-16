7db6cea358e8cbca5d903422a019c06642278b2a
NathanDeMaria <naterstd@gmail.com>
2015-05-16 18:14:09 -0500

    Redo get.R to save to a sqlite db, remove duplicates as it goes, be all around better

--- data/get.R
+++ data/get.R
@@ -4,8 +4,14 @@ library(data.table)
 library(dplyr)
 library(lubridate)
 
-get_comments <- function(max_date=NULL) {
+get_comments <- function(db_file='data/comments.db') {
+	# if db_file already exists, appends to that db
+	# otherwise, creates a new one
+	
 	source('config/app_config.R', local = T)
+	source('data/sqlite.R', local = T)
+	
+	sqlite <- create_sqlite_accessor(db_file)
 	
 	# do me/inbox
 	base_url <- 'https://graph.facebook.com'
@@ -36,30 +42,36 @@ get_comments <- function(max_date=NULL) {
 			)
 	}
 	
-	date_ok <- function() {
-		if (is.null(max_date)) {
-			return(T)
-		}
-		last_frame <- comments_frames[[length(comments_frames)]]
-		times <- ymd_hms(last_frame$time)
-		min(times) > max_date - days(1) # extra day of overlap just in case
+	list_to_df <- function(comments_list) {
+		comments_frames <- lapply(comments_list$data, parse_comment)
+		data.table(rbind_all(comments_frames))
 	}
 	
-	comments_frames <- lapply(comments_list$data, parse_comment)
+	comments_dt <- list_to_df(comments_list)
+	
+	if (file.exists(db_file)) {
+		sqlite$append_table(comments_dt, table_name = 'comment')
+	} else {
+		sqlite$create_table(comments_dt, table_name = 'comment', id = 'id')
+	}
+	num_added <- nrow(comments_dt)
+	
 	i <- 1
-	while (!is.null(comments_list$paging$`next`) && date_ok()) {
+	while (!is.null(comments_list$paging$`next`) && num_added > 0) {
 		comments_list <- content(GET(comments_list$paging$`next`))
 		Sys.sleep(2)
 		if (!is.null(comments_list$error)) {
 			warning(paste(comments_list$error$message, "\nYou probably did not get all the messages"))
 		}
-		comments_frames <- c(comments_frames, lapply(comments_list$data, parse_comment))
+		comments_dt <- list_to_df(comments_list)
+		if (nrow(comments_dt) > 0) {
+			num_added <- sqlite$append_table(comments_dt, table_name = 'comment')
+		}
 		cat(sprintf('Round %i\n', i))
 		i <- i + 1
 	}
 	
-	comments <- data.table(rbind_all(comments_frames))
-	comments[,time := ymd_hms(time)]
-	setkey(comments, id)
+	comments <- data.table(sqlite$read_table('comment'))
+	setkey(comments, 'id')
 	comments
 }
