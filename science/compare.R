c97d90033540e6cc4d2ec105b46e3d2fac069136
NathanDeMaria <naterstd@gmail.com>
2015-05-30 19:55:08 -0500

    Add more options to eval and running some forests in compare

--- science/compare.R
+++ science/compare.R
@@ -2,6 +2,10 @@ source('data/load.R')
 source('science/eval.R')
 
 source('science/models/textcat.R')
+source('science/models/random_forest.R')
 
 
-evaluate(textcat_model)
+textcat_score <- evaluate(textcat_model, min_messages = 0)
+small_forest <- evaluate(random_forest_model, n = 25, min_messages = 0)
+large_forest <- evaluate(random_forest_model, n = 50, min_messages = 0)
+largest_forest <- evaluate(random_forest_model, n = 100, min_messages = 0)
--- science/eval.R
+++ science/eval.R
@@ -2,10 +2,12 @@
 create_train_test <- function(comments, min_messages=250, balance=F) {
 	train_prop <- 0.8
 	
+	# find people who have sent the minimum number of messages
 	candidates <- comments[,list(count = length(text)), by = name][count > min_messages]$name
 
 	
 	if (balance) {
+		# all people have the same proportions in the train and test sets
 		top_ind <- floor(min_messages * train_prop)
 		
 		ind_groups <- sapply(candidates, function(candidate) {
@@ -18,6 +20,8 @@ create_train_test <- function(comments, min_messages=250, balance=F) {
 		sets <- list(train = comments[unlist(ind_groups['train',]),], 
 					 test = comments[unlist(ind_groups['test',]),])
 	} else {
+		# people have the same proportion of messages in the train 
+		# and test sets as they as they do in the real dataset
 		valid <- comments[name %in% candidates]
 		valid <- valid[sample(1:nrow(valid)),]
 		top_ind <- floor(train_prop * nrow(valid))
@@ -28,15 +32,22 @@ create_train_test <- function(comments, min_messages=250, balance=F) {
 	sets
 }
 
-evaluate <- function(model_generator, balance=F, verbose=F, ...) {
-	tt <- create_train_test(comments, balance = balance)
+evaluate <- function(model_generator, min_messages = 250, balance=F, even_weighted=T, verbose=F, ...) {
+	tt <- create_train_test(comments, min_messages = min_messages, balance = balance)
 	predictor <- model_generator(tt$train, ...)
-	predictions <- predictor(tt$test)
-	
+	tt$test$prediction <- predictor(tt$test)
+
 	if (verbose) {
 		# print confusion matrix
-		print(table(predictions, tt$test$name))
+		print(table(tt$test$prediction, tt$test$name))
 	}
 	
-	mean(predictions == tt$test$name, na.rm = T)
+	if (even_weighted) {
+		# final accuracy is reported as proportion correct if each person was equally weighted
+		scores <- tt$test[,list(score = mean(name == prediction, na.rm = T)), by = name]
+		mean(scores$score)
+	} else {
+		# final accuracy is just proportion of total correct
+		mean(tt$test$prediction == tt$test$name, na.rm = T)
+	}
 }
